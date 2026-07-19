fa0a12d396de69de3a8025ebb5f5f04b810f5da9
Charvie G Ganju <charvieganjoo@gmail.com>
2026-07-19 22:09:33 +0530

    Update Lc_26.java

--- Lc_26.java
+++ Lc_26.java
@@ -1,2 +1,24 @@
-public class Lc_26 {
+import java.util.Arrays;
+
+class Lc_26 {
+    public static int removeDuplicates(int[] nums) {
+        int k = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+  
+    public static void main(String[] args) {
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        int k = removeDuplicates(nums);
+        System.out.println("Number of unique elements: " + k);
+        System.out.print("Array without duplicates: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
 }
